faab7ddd776ab15a5e9cef8a3792c0ef41d16f0b
johnathansmithpro <156258546+johnathansmithpro@users.noreply.github.com>
2024-03-03 17:36:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,18 +5,18 @@
 
   "port": "3000",
   
-  "clientId": "1191805925888110683",
-  "guildId": "1187745297095458877",
+  "clientId": "1170025049596641414",
+  "guildId": "1169420697093799956",
   "globalDeploy": false, 
 
-  "status": "Subscribe to CyberSecDiscord",
-  "website": "https://youtube.com/@CyberSecDiscord",
-  "banner": "https://media.discordapp.net/attachments/960440723369517106/996423763522506782/rainbow-border.gif",
-  "footer": "Subscribe to CyberSecDiscord",
+  "status": "Draxco on top",
+  "website": "https://discord.gg/AUjabkBvpr",
+  "banner": "https://cdn.discordapp.com/attachments/1212204029040664596/1213977620702822470/Lil_Uzi_Vert_-_Pink_Tape.png?ex=65f76fc3&is=65e4fac3&hm=1ef0777740db6dd2a382e220ffd3962b6be1a151c1439852334977876f4f00b9&",
+  "footer": "Draxco Methods",
 
-  "genChannel": "1191806304642150501",
-  "premiumChannel": "1191806320614055946",
-  "genCooldown": "5",
+  "genChannel": "1169615667725602847",
+  "premiumChannel": "1169615667725602847",
+  "genCooldown": "7200",
   "premiumCooldown": "3",
 
   "color": {
